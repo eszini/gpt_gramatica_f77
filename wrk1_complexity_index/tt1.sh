a1431faa957e7651a9e4139a094768656692e966
ernesto <eszini@gmail.com>
2025-04-29 00:14:29 -0300

    gramatica_f77 - src inicial

---
+++ wrk1_complexity_index/tt1.sh
@@ -0,0 +1,6 @@
+# 
+# lee todo el codebase, y ....
+#
+
+./tgram -v -opciones=d5i1 -debug=1 -zap=1 -util=1 -opt=0 -inp=lr1 -dato=repo4 -out=list_complex.txt -ou2=zfor.txt -ou3=zf90.txt -ou4=plan.txt -aux=lfor.txt -log=lf90.txt -ou5=mapa.txt -ou6=error.txt -dbg=debug.lst -ou7=corregir.txt
+
